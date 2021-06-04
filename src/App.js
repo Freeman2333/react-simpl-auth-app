6abafae62b40280b19c2d13bef805c4d807eaf79
s.popov <s.popov@indevlab.com>
2021-06-04 13:36:18 +0300

    fisrt

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react';
+import { BrowserRouter, Switch,Route } from 'react-router-dom';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Preferences from './components/Preferences';
+import Login from './components/Login'
+
+function App() {
+  const [token, setToken] = useState()
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+          <Route path="/preferences">
+            <Preferences/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
